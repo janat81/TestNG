3e3798737d8bf6dfd074e718157c0e2827847a91
zhanat güneş <guneszhanat@gmail.com>
2023-01-18 06:54:29 +0300

    testng

--- src/test/java/techproed/tests/paralleltesting/Day22_ManagerLogin.java
+++ src/test/java/techproed/tests/paralleltesting/Day22_ManagerLogin.java
@@ -8,16 +8,16 @@ import techproed.utilities.ReusableMethods;
 import java.time.Duration;
 public class Day22_ManagerLogin {
     @Test
-    public void customerLogin(){
+    public void customerLogin() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://www.bluerentalcars.com/login");
         driver.findElement(By.id("formBasicEmail")).sendKeys("sam.walker@bluerentalcars.com");
-        ReusableMethods.waitFor(2);
+        Thread.sleep(2000);
         driver.findElement(By.id("formBasicPassword")).sendKeys("c!fas_art");
-        ReusableMethods.waitFor(2);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.quit();
     }
