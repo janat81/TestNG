ad583c9719de7f7c51ce3798b21ae02a801f1da8
zhanat güneş <guneszhanat@gmail.com>
2023-01-19 02:54:47 +0300

    testng

--- src/test/java/techproed/tests/Day16_Annotations.java
+++ src/test/java/techproed/tests/Day16_Annotations.java
@@ -5,6 +5,13 @@ public class Day16_Annotations {
     suite > test > group > class > method
     @Test : creates test case
     @Before and @After : 10 before and after annotations. The are used to control FLOW and DESIGN of the tests
+    @Ignore : skip test case
+    @Test(enable=false) : Disable the test case. By default enable=true.
+    @Test(priority=number) : prioritize/order test case execution.
+    NOTE: Tests that has no priority parameter has a priority of 0. Test(priority=0)
+    TestNG test cases runs in alphabetical order by default.
+    We should use priority when there are multiple test cases in the same class.
+    -3, 0(ALSO TESTS WITH NO PRIORITY), 4, 9
      */
     @BeforeSuite
     public void beforeSuite(){
@@ -38,8 +45,7 @@ public class Day16_Annotations {
     public void afterMethod(){
         System.out.println("After Method");
     }
-
-    @Test
+    @Test(priority = 2)
     public void test1(){
         System.out.println("test1");
     }
@@ -48,16 +54,20 @@ public class Day16_Annotations {
     public void test2(){
         System.out.println("test2");
     }
-    @Test
+    @Test(enabled = false)
     public void test3(){
         System.out.println("test3");
     }
-    @Test
+    @Test(priority = 3)
     public void test4(){
         System.out.println("test4");
     }
-    @Test
+    @Test(priority = 1,groups = "minor-regression-group")
     public void test5(){
         System.out.println("test5");
+    }
+    @Test(groups = "minor-regression-group")
+    public void test6(){
+        System.out.println("test6");
     }
 }
